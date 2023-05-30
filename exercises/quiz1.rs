3e4948e37aefb51fac267a86cfda7070ad5227e9
Seundede <omadoyedede0@gmail.com>
2023-05-30 11:42:14 +0100

    feat: quiz_1 task

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,18 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num:i32) -> i32 {
+    let apple_cost:i32 = 2;
+    if num > 40 {
+        num
+    } else {
+        num * apple_cost
+    }
+
+
+}
 
 // Don't modify this function!
 #[test]
